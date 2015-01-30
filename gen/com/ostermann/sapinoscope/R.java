b5e1e9d71ceb26d66da238929198d92f5faac905
Damien <damien.klainguer@live.fr>
2015-01-30 11:43:47 +0100

    Ajout fenetre "Ajout_Sapin"

--- gen/com/ostermann/sapinoscope/R.java
+++ gen/com/ostermann/sapinoscope/R.java
@@ -54,16 +54,18 @@ public final class R {
         public static final int txt_messageAlerte_secteurName_txt=0x7f080013;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_parcelle=0x7f030001;
-        public static final int activity_secteur=0x7f030002;
-        public static final int message_alerte=0x7f030003;
+        public static final int activity_ajout_sapin=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_parcelle=0x7f030002;
+        public static final int activity_secteur=0x7f030003;
+        public static final int message_alerte=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int message_alerte=0x7f070001;
-        public static final int parcelle=0x7f070002;
-        public static final int secteur_activity=0x7f070003;
+        public static final int ajout_sapin=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int message_alerte=0x7f070002;
+        public static final int parcelle=0x7f070003;
+        public static final int secteur_activity=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -79,6 +81,7 @@ public final class R {
         public static final int parcelle=0x7f050007;
         public static final int secteur=0x7f05000e;
         public static final int synchronisation=0x7f050005;
+        public static final int title_activity_ajout_sapin=0x7f050010;
         public static final int title_activity_message_alerte=0x7f05000d;
         public static final int title_activity_parcelle=0x7f050006;
         public static final int title_activity_secteur_activity=0x7f05000c;
